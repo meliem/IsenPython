477176c2f55e9dc7e359ce5749f0c3f45bd0b635
elie <elie@workstation.local>
2025-02-25 16:12:18 +0100

    -

---
+++ src/ex1.py
@@ -0,0 +1,8 @@
+while(1):
+    j = int(input("entre un nombre pelo"))
+    if j%2 == 0:
+        print("c pair mon gadjo")
+    elif j == -1:
+        break
+    else:
+        print("c impair pelo")
