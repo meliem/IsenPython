4a7f386227bd105fa7bda205198f5878b16b0534
elie <elie@workstation.local>
2025-02-25 16:18:23 +0100

    -

---
+++ src/ex2.py
@@ -0,0 +1,13 @@
+temps = int(input("Vazy frr, envoie un blase de secondes là : "))
+
+jrs = temps // 86400
+reste = temps % 86400  
+
+hrs = reste // 3600
+reste = reste % 3600  
+
+minos = reste // 60
+secs = reste % 60
+
+# On balance tout ça vénère  
+print(f"{jrs} jrs, {hrs} hrs, {minos} minos, {secs} secs, la street validé frr")
